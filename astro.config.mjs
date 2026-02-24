b9a4cd52c36f1e1b218767365d99d9fbd4a2fdea
kaylapratt <kaylapratt@protonmail.com>
2026-02-24 14:49:45 -0500

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/deans-okinawan',
+  base: '/deans-okinawan/',
   integrations: [react(), icon()],
   output: 'static',
 });
