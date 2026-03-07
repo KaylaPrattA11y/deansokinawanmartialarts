cd611c06aad35cf69c7d817124fe96c778c2e462
kaylapratt <kaylapratt@protonmail.com>
2026-03-07 17:56:10 -0500

    Revert "[cms] get public site url for tinacms media url construction"
    This reverts commit c2f7be0e2be329bb1c847dfdbd2f659653b65ed1.

--- tina/config.ts
+++ tina/config.ts
@@ -5,7 +5,6 @@ import {
 } from "tinacms-authjs/dist/tinacms";
 
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
-const siteUrl = process.env.TINA_PUBLIC_SITE_URL ?? "";
 
 // const branch =
 //   process.env.GITHUB_BRANCH ||
@@ -16,7 +15,7 @@ const siteUrl = process.env.TINA_PUBLIC_SITE_URL ?? "";
 export default defineConfig({
   ...(isLocal ? {} : { 
     contentApiUrlOverride: "/api/tina/gql",
-    mediaUrlOverride: `${siteUrl}/api/tina/media`,
+    mediaUrlOverride: "/api/tina/media",
   }),
   authProvider: isLocal
     ? new LocalAuthProvider()
