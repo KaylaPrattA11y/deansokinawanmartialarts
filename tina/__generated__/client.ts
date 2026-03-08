fff51dde80108216c3139c6c02500fb81aec8ac7
kaylapratt <kaylapratt@protonmail.com>
2026-03-08 12:17:00 -0400

    [cms] clent update

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/eric-kayla/Kayla/Website Projects/deans-okinawan/tina/__generated__/.cache/1772985988256', url: 'https://content.tinajs.io/2.2/content/6ae2b788-2470-438a-88cd-df7ccf145a29/github/main', token: 'c2ccfbfd51bf7cfd296dea63078179324819e35b', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'c2ccfbfd51bf7cfd296dea63078179324819e35b', queries,  });
 export default client;
   
