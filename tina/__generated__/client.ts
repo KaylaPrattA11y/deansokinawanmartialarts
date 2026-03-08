2aa3caf1cc4e761071aefce4e2c91b20b4ff99ae
kaylapratt <kaylapratt@protonmail.com>
2026-03-08 12:08:06 -0400

    [cms] update content token

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/eric-kayla/Kayla/Website Projects/deans-okinawan/tina/__generated__/.cache/1772984709141', url: 'https://content.tinajs.io/2.2/content/6ae2b788-2470-438a-88cd-df7ccf145a29/github/main', token: '94ac25cf53c29f5dfe3d74b03e6a43a5e32f9c28', queries,  });
+export const client = createClient({ cacheDir: '/home/eric-kayla/Kayla/Website Projects/deans-okinawan/tina/__generated__/.cache/1772985988256', url: 'https://content.tinajs.io/2.2/content/6ae2b788-2470-438a-88cd-df7ccf145a29/github/main', token: 'c2ccfbfd51bf7cfd296dea63078179324819e35b', queries,  });
 export default client;
   
